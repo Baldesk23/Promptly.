da3e016d81551b5bb955ea66c8954be6f08776e1
Baldesk23 <131199512+Baldesk23@users.noreply.github.com>
2025-10-19 01:58:45 -0300

    cambii

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
-const ContentSecurityPolicy = "
-default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.gstatic.com https://www.googleapis.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: https:; connect-src 'self' https://identitytoolkit.googleapis.com https://firestore.googleapis.com https://firebasestorage.googleapis.com https://firebaserules.googleapis.com; font-src https://fonts.gstatic.com; frame-src https://accounts.google.com;
-"
+const ContentSecurityPolicy = `
+default-src 'self';
+script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.gstatic.com https://www.googleapis.com;
+style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+img-src 'self' data: https:;
+connect-src 'self' https://identitytoolkit.googleapis.com https://firestore.googleapis.com https://firebasestorage.googleapis.com https://firebaserules.googleapis.com;
+font-src https://fonts.gstatic.com;
+frame-src https://accounts.google.com;
+`;
 
 const securityHeaders = [
   { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
@@ -11,7 +17,7 @@ const securityHeaders = [
   { key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload' },
   { key: 'Permissions-Policy', value: 'geolocation=(), microphone=()' },
   { key: 'Content-Security-Policy', value: ContentSecurityPolicy.replace(/\n/g, ' ') },
-]
+];
 
 const nextConfig = {
   reactStrictMode: true,
@@ -24,8 +30,8 @@ const nextConfig = {
         source: '/(.*)',
         headers: securityHeaders,
       },
-    ]
+    ];
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
